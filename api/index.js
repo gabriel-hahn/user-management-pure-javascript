88359fa3b6064c61b392953a19b97c05601bb77b
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-07-12 22:46:03 -0300

    Validator file
    Validator file for request

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(expressValidator());
 
 //App into all the routes.
 consign().include('routes').include('utils').into(app);
